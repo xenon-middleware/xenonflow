39ad7964c2b5d6f5fddd4ff5d36e754f576193e0
Berend Weel <b.weel@esciencecenter.nl>
2021-10-23 19:47:02 +0200

    Update xenon-flow-runner.sh

--- xenon-flow-runner.sh
+++ xenon-flow-runner.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-. $SCRIPT_DIR/cwltest-venv/bin/activate && python3 $SCRIPT_DIR/xenon-flow-runner.py $@
+python3 $SCRIPT_DIR/xenon-flow-runner.py $@
